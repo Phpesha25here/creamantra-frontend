658678cd7329bb73589c0d3e0fbadde585ba88cb
Esha <lokareesha2.5@gmail.com>
2026-04-23 14:28:28 +0530

    update

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -6,7 +6,7 @@ import { toast } from "react-hot-toast";
 
 export const AppContext = createContext();
 
-axios.defaults.baseURL = "https://creamantra-backend.onrender.com";
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 axios.defaults.withCredentials = true;
 
 const AppContextProvider = ({ children }) => {
